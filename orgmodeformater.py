20a1fff704492b85fdad71ef8d8e940f6b5c0b71
Nicolas Vaughan <nivaca@fastmail.com>
2021-09-01 14:08:26 -0500

    first commit

---
+++ orgmodeformater.py
@@ -0,0 +1,56 @@
+import os
+import binascii
+from shutil import copyfile
+
+orgtags = ':latin:'
+directory = ''  # local directory
+extensions = ['.org']
+
+
+def generate_id():
+    def genhex(n):
+        return str(binascii.b2a_hex(os.urandom(n)).decode())
+
+    return f"{genhex(4)}-{genhex(2)}-{genhex(2)}-{genhex(2)}-{genhex(6)}".lower()
+
+
+def get_files():
+    files = []
+    allfilenames = os.listdir(directory + '.')
+    for fi in allfilenames:
+        ext = os.path.splitext(fi)[1]
+        # check if file of proper extension
+        if ext.lower() in extensions:
+            files.append(fi)
+    # returns a lists of file objects
+    return files
+
+
+def formathead():
+    return f":PROPERTIES:\n" \
+           f":ID:       {generate_id()}\n" \
+           f":END:\n" \
+           f"#+filetags: {orgtags}\n" \
+           f"#+title: \n\n"
+
+
+def insertid(fn):
+    copyfile(fn, f'{fn}.bak')  # make a backup file
+    with open(fn, mode="w") as destfile:
+        destfile.write(formathead())
+        with open(f'{fn}.bak') as origfile:
+            for line in origfile:
+                if '@' in line:
+                    continue
+                destfile.write(f"{line}")
+
+
+def main():
+    files = get_files()
+    for fn in files:
+        print(f"Processing {fn}...")
+        insertid(fn)
+
+
+if __name__ == "__main__":
+    main()
